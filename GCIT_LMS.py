26752b389ded9dd1d48db5f46e3a1d62e93e99c3
Joe Corapi <joe.corapi@smoothstack.com>
2019-12-20 14:02:41 -0500

    remove recursive looping for STATE function

--- GCIT_LMS.py
+++ GCIT_LMS.py
@@ -1,3 +1,4 @@
+from enum import Enum
 import mysql.connector
 
 cnx = mysql.connector.connect(user='root', password='jacmsq5903',
@@ -39,54 +40,101 @@ def displayResultSet(resultSet, quitMenu=False):
     if quitMenu == True:
         print(str(i) + ') Quit to menu?')    
 
+def getValidInput(validLength):
+    while True:
+        try:
+            userSelection = int(input())
+        except ValueError:
+            print('Please make your selection using the number pad.')  
+            continue
+        if (userSelection < 1) or (userSelection > validLength):
+            print('Please select a value in the numbered list.') 
+            continue
+        else:
+            break
+    return userSelection
+
+class State(Enum):
+    MAIN = 1
+    LIB1 = 2
+    LIB2 = 3
+    LIB3 = 4
+    BORR1 = 5
+    BORR2 = 6
+    ADMIN =  7
+
 def libMenuSelector(screenCode):
     case={
-        'main': main,
-        'lib1': lib1,
-        'lib2': lib2,
+        State.MAIN: main,
+        State.LIB1: lib1,
+        State.LIB2: lib2,
         #'lib3': lib3,
-        'borr1': borr1,
+        #'borr1': borr1,
         #'borr2': borr2,
-        'admin': admin
+        #'admin': admin
         }
-    case.get(screenCode, main)()
+    case.get(screenCode, State.MAIN)()
 
 def main():
     def selectionHandler(user):
         return {
-            '1':'lib1',
-            '2':'admin',
-            '3':'borr1'
-        }.get(user, 'main')
+            1: State.LIB1,
+            2: State.ADMIN,
+            3: State.BORR1
+        }.get(user, State.MAIN)
 
     print("Welcome to the GCIT Library Management System. Which category of a user are you?\n")
     print("1) Librarian \n2) Administrator \n3) Borrower \n")
-    userSelection = input()
-    libMenuSelector(selectionHandler(userSelection))
+
+    global activeState
+    activeState = selectionHandler(getValidInput(3))
+    return (activeState)       
 
 def lib1():
     def selectionHandler(selection):
         return {
-            '1':'lib2',
-            '2':'main'
-        }.get(selection, 'lib1')
+            1: State.LIB2,
+            2: State.MAIN
+        }.get(selection, State.LIB1)
 
     print("1) Enter Branch you manage \n2) Quit to previous \n")
-    userSelection = input()
-    libMenuSelector(selectionHandler(userSelection))
+    global activeState
+    activeState = selectionHandler(getValidInput(2))
+    return (activeState)
 
 def lib2():
     branchNameList = getQueryColumn('getBranchNames', 0)
-    displayResultSet(listCombiner(branchNameList, getQueryColumn('getBranchAddress', 0)), True)
+    branchAddressList = getQueryColumn('getBranchAddress', 0)
+    displayResultSet(listCombiner(branchNameList, branchAddressList), True)
 
-#def lib3():
+    print(branchNameList)
+    userSelection = getValidInput(len(branchNameList)+1)
+
+    if userSelection == (len(branchNameList)+1):
+        global activeState
+        activeState = State.LIB1
+        return(activeState)
+    else:
+        lib3(branchNameList[userSelection-1], branchAddressList[userSelection-1])
+
+def lib3(branchName, branchAddress):
+    global activeState 
+    activeState = State.LIB3
+    print('1) Update the details of the Library \n2) Add copies of Book to the Branch \nQuit to previous \n')
+    
 def borr1():
     print('Welcome Borrower')
 #def borr2():
 def admin():  
     print('Welcome Admin')  
 
-libMenuSelector(0)
+def libraryManagementSystemApplication():
+    while True:
+        libMenuSelector(activeState)
+
+activeState = State.MAIN
+libraryManagementSystemApplication()
+
 #preparedStatement('Select branchname from tbl_library_branch')
 #print('---------SINGLE DATA COLUMN TEST-------------')
 #displayResultSet(getQueryColumn('getBranches', 0), True)
